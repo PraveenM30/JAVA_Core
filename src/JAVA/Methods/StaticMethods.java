f42e63e39bc970dd92acbf06deac09f090296643
Praveen M <praveen.mohansundar9@gmail.com>
2025-07-25 16:47:44 +0530

    static updated

--- src/JAVA/Methods/StaticMethods.java
+++ src/JAVA/Methods/StaticMethods.java
@@ -3,7 +3,11 @@ package JAVA.Methods;
 public class StaticMethods {
 
   static String a="It's static";
+  String b="It's Non static";
+
     public static void main(String args[]){
         System.out.println(a);
+        StaticMethods obj=new StaticMethods();
+        System.out.println(obj.b);
     }
 }
